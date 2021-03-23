e2323a9fcf6f5e7fd99b4b7a265eb84701b9069e
Bilal <bilal.m.fazlani@gmail.com>
2021-03-23 18:18:45 +0000

    Update scopt to 4.0.1

--- project/Libs.scala
+++ project/Libs.scala
@@ -10,7 +10,7 @@ object Akka {
 
 object Libs {
   lazy val munit = "org.scalameta" %% "munit" % "0.7.22"
-  lazy val scopt = ("com.github.scopt" %% "scopt" % "4.0.0").withCrossVersion(CrossVersion.for3Use2_13)
+  lazy val scopt = ("com.github.scopt" %% "scopt" % "4.0.1").withCrossVersion(CrossVersion.for3Use2_13)
   lazy val `scala-csv` = ("com.github.tototoshi" %% "scala-csv" % "1.3.7").withCrossVersion(CrossVersion.for3Use2_13)
   lazy val `scala-rainbow` = "com.github.bilal-fazlani" % "scala-rainbow" % "1.0.0"
 }
